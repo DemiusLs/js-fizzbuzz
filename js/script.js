e6d17f0ab0cf85c6b56dbf1acaa166ba51e561dd
DemiusLs <dloss320@gmail.com>
2025-04-15 17:49:02 +0200

    fix: project fixed

--- js/script.js
+++ js/script.js
@@ -4,21 +4,24 @@ for (let i = 1; i <= 100; i++) {
 
     message = i;
 
-    if ((i % 3 === 0) || (i % 5 === 0)) {
+    if (i % 3 === 0) {
 
-        message = "FizzBuzz";
-        if ((i % 3 === 0) && !(i % 5 === 0)) {
+        message = "Fizz";
+    } else if (i % 5 === 0) {
 
-            message = "Fizz";
-        } else if ((i % 5 === 0) && !(i % 3 === 0)) {
-
-            message = "Buzz";
-        }
-
+        message = "Buzz";
     }
 
-
-
+    if ((i % 5 === 0) && (i % 3 === 0)) {
+        message = "FizzBuzz";
+    }
 
     console.log(message)
 }
+
+
+
+
+
+
+
