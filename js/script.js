e23cfe0e39a819630f792af07e73d859febeb4fb
DemiusLs <dloss320@gmail.com>
2025-04-15 18:05:49 +0200

    fix: if case fixed

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ for (let i = 1; i <= 100; i++) {
 
     message = i;
 
-    if (i % 3 === 0) {
+
+
+    if ((i % 5 === 0) && (i % 3 === 0)) {
+        message = "FizzBuzz";
+    } else if (i % 3 === 0) {
 
         message = "Fizz";
     } else if (i % 5 === 0) {
 
         message = "Buzz";
-    }
-
-    if ((i % 5 === 0) && (i % 3 === 0)) {
-        message = "FizzBuzz";
     }
 
     console.log(message)
